0ff87796ba54b8cc6981e89b9237f0d6ec0e73ee
garyongguanjie <43882298+garyongguanjie@users.noreply.github.com>
2021-02-22 11:21:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,14 @@
 let trie;
 (async()=>{
-    let response = await fetch("https://garyongguanjie.github.io/entrie/trie.json");
-    trie = await response.json();
-    console.log(trie);
+    let trie = entrie.get();
 })();
-function entrie(trie,prefix,distance=2){
-    function dfs(trie,prefix,arr,depth){
-        if (depth>distance){
-            return
-        }
-        for(let k in trie){
-            let nextPrefix;
-            if(k==="_"){
-                arr.push({word:prefix,freq:trie["freq"]})
-            }else{
-                nextPrefix = prefix+k;
-                dfs(trie[k],nextPrefix,arr,depth+1);
-            }
-        }
-    }
-    let temp = trie;
-    for(let c of prefix){
-        if(c in temp){
-            temp = temp[c];
-        }else{
-            console.log("Word does not exist");
-            return undefined;
-        }
-    }
-    // get all possible words given prefix
-    let arr = [];
-    dfs(temp,prefix,arr,0);
-    arr.sort((a,b)=>b.freq-a.freq);
-    return arr.map(x=>x.word)
-}
 function onclickhandler(e) {
     let prefix = e.value;
+    let topElement = document.getElementById("words");
+    topElement.innerHTML = '';
+    if(prefix==="")return;
     if(trie!==undefined){
-        let topElement = document.getElementById("words");
-        topElement.innerHTML = '';
-        let words = entrie(trie,prefix,4);
+        let words = entrie.find(trie,prefix,4);
         if(words==undefined)return;
         for(word of words){
             var div = document.createElement("div");
