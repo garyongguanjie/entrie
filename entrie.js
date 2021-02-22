0519925bcd597181164db665cf80fd02057e9b51
garyongguanjie <43882298+garyongguanjie@users.noreply.github.com>
2021-02-22 11:18:28 +0800

    Update entrie.js

--- entrie.js
+++ entrie.js
@@ -1,30 +1,37 @@
-function entrie(trie,prefix,distance=2){
-    function dfs(trie,prefix,arr,depth){
-        if (depth>distance){
-            return
-        }
-        for(let k in trie){
-            let nextPrefix;
-            if(k==="_"){
-                arr.push({word:prefix,freq:trie["freq"]})
-            }else{
-                nextPrefix = prefix+k;
-                dfs(trie[k],nextPrefix,arr,depth+1);
+const entrie = {
+    get:async function(){
+        let response = await fetch("https://garyongguanjie.github.io/entrie/trie.json");
+        trie = await response.json();
+        console.log(trie);
+    },
+    find:function(trie,prefix,distance=2){
+        function dfs(trie,prefix,arr,depth){
+            if (depth>distance){
+                return
+            }
+            for(let k in trie){
+                let nextPrefix;
+                if(k==="_"){
+                    arr.push({word:prefix,freq:trie["freq"]})
+                }else{
+                    nextPrefix = prefix+k;
+                    dfs(trie[k],nextPrefix,arr,depth+1);
+                }
             }
         }
-    }
-    let temp = trie;
-    for(let c of prefix){
-        if(c in temp){
-            temp = temp[c];
-        }else{
-            console.log("Word does not exist");
-            break
+        let temp = trie;
+        for(let c of prefix){
+            if(c in temp){
+                temp = temp[c];
+            }else{
+                console.log("Word does not exist");
+                break
+            }
         }
+        // get all possible words given prefix
+        let arr = [];
+        dfs(temp,prefix,arr,0);
+        arr.sort((a,b)=>b.freq-a.freq);
+        return arr.map(x=>x.word)
     }
-    // get all possible words given prefix
-    let arr = [];
-    dfs(temp,prefix,arr,0);
-    arr.sort((a,b)=>b.freq-a.freq);
-    return arr.map(x=>x.word)
 }
